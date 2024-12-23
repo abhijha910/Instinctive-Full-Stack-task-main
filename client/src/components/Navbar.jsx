a06f759f144c3a4b1bea157110ec093599e13635
Abhyanand Jha <123495322+abhijha910@users.noreply.github.com>
2024-12-23 13:01:18 +0530

    Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import { IoMdSearch } from "react-icons/io";
 import { LuMessageSquareMore } from "react-icons/lu";
-import { GoBell } from "react-icons/go";
+import { FiBell } from "react-icons/fi";
 import { MdHelpOutline } from "react-icons/md";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const Navbar = () => {
   return (
@@ -19,18 +25,41 @@ const Navbar = () => {
       <div className="md:w-full flex items-center gap-6 justify-end text-gray-500">
         <div className="items-center gap-8 justify-end  text-gray-500 hidden md:flex">
           <LuMessageSquareMore size={24} />
-          <GoBell size={24} />
+          <FiBell size={24} />
           <MdHelpOutline size={24} />
         </div>
-        <div className="justify-end flex items-center gap-4">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span className="text-lg font-semibold text-black hidden md:block">
-            Adeliny
-          </span>
-        </div>
+        <Popover>
+          <PopoverTrigger>
+            <div className="justify-end flex items-center gap-4 cursor-pointer">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <span className="text-lg font-semibold text-black hidden md:block">
+                Adeliny
+              </span>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="p-2 flex flex-col gap-4">
+              <div className="flex items-center gap-4">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <div>
+                  <h1 className="text-lg font-semibold">Adeliny</h1>
+                </div>
+              </div>
+              <div className="items-center justify-between text-gray-500 md:hidden flex">
+                <LuMessageSquareMore size={20} />
+                <FiBell size={20} />
+                <MdHelpOutline size={20} />
+              </div>
+              <Button variant="outline"> Logout </Button>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
     </div>
   );
